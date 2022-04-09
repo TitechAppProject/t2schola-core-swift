6b1aa796bd41f63e17b3a162f969bca2dfd872b9
nanashiki <nanashiki.app@gmail.com>
2022-04-09 10:34:34 +0900

    Update UserEnrolCourseResponse

--- Sources/T2ScholaCoreSwift/Request/UserEnrolCourseRequest.swift
+++ Sources/T2ScholaCoreSwift/Request/UserEnrolCourseRequest.swift
@@ -24,12 +24,34 @@ struct UserEnrolCoursesRequest: RestAPIRequest {
 public typealias UserEnrolCoursesResponse = [UserEnrolCourseResponse]
 
 public struct UserEnrolCourseResponse {
-    public let id: Int
-    public let shortname: String
-    public let fullname: String
-    public let category: Int
-    public let startdate: Date
-    public let enddate: Date
+    public let id: Int // Course id.
+    public let fullname: String // Course full name.
+    public let displayname: String? // Course display name.
+    public let shortname: String // Course short name.
+    public let summary: String // Summary.
+    public let summaryformat: Int // Summary format (1 = HTML, 0 = MOODLE, 2 = PLAIN or 4 = MARKDOWN).
+    public let categoryid: Int? // Course category id.
+    public let idnumber: String? // Id number of course.
+    public let visible: Int? // 1 means visible, 0 means not yet visible course.
+    public let format: String? // Course format: weeks, topics, social, site.
+    public let showgrades: Bool? // True if grades are shown, otherwise false.
+    public let lang: String? // Forced course language.
+    public let enablecompletion: Bool? // True if completion is enabled, otherwise false.
+    public let startdate: Date? // Timestamp when the course start.
+    public let enddate: Date? // Timestamp when the course end.
+    public let enrolledusercount: Int? // Number of enrolled users in this course.
+    public let completionhascriteria: Bool? // If completion criteria is set.
+    public let completionusertracked: Bool? // If the user is completion tracked.
+    public let progress: Int? // Progress percentage.
+    public let completed: Bool? // Whether the course is completed.
+    public let marker: Int? // Course section marker.
+    public let lastaccess: Int? // Last access to the course (timestamp).
+    public let isfavourite: Bool? // If the user marked this course a favourite.
+    public let hidden: Bool? // If the user hide the course from the dashboard.
+//    public let overviewfiles?: CoreWSExternalFile[]
+    public let showactivitydates: Bool? // @since 3.11. Whether the activity dates are shown or not.
+    public let showcompletionconditions: Bool? // @since 3.11. Whether the activity completion conditions are shown or not.
+    public let category: Int? // Course category id.
 }
 
 extension UserEnrolCourseResponse: Codable {}
